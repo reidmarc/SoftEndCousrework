0d777861164660d2cdd836fddbebb885c69be584
Marc Reid <03001588@live.napier.ac.uk>
2017-11-04 11:16:05 +0000

    Project start.

---
+++ SE Coursework/Menu.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SE_Coursework
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        Validation validation = new Validation();
+
+
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        #region Click Events
+
+        private void manuallyInputButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button works!!! OMG");
+        }
+
+        #endregion
+
+
+
+
+
+
+        #region Exit Button
+
+        // Method which handles the 'Exit Application' button being clicked
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Calls the method ExitApplicationValidation() from the Validation class.
+            validation.ExitApplicationValidation();
+        }
+
+        #endregion
+
+       
+    }
+}
---
+++ SE Coursework/Validation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SE_Coursework
+{
+    public class Validation
+    {
+
+
+
+
+
+
+
+
+
+
+
+        #region User's Choice Validation Methods
+
+        // Method which displays a messagebox asking the user if they are sure they want to exit the application
+        // And gives them the option of answering 'yes' or 'no'
+        public void ExitApplicationValidation()
+        {
+            MessageBoxResult yesOrNo = MessageBox.Show("Are you sure you want to exit the application?", "Exit Application", MessageBoxButton.YesNo);
+
+            if (yesOrNo == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+
+            else
+            {
+                return;
+            }
+        }
+
+        #endregion
+
+
+
+    }
+}
